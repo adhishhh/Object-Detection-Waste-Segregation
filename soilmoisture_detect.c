bf4032dcf0000dfa61099e462e2910e28de213f4
adhishvairagade <adhishvairagade20@gmail.com>
2023-07-22 03:26:27 +0530

    Create soilmoisture_detect.c

---
+++ soilmoisture_detect.c
@@ -0,0 +1,33 @@
+//adhishvairagade
+/*
+This Arduino code uses a Servo motor and a soil moisture sensor to monitor soil moisture levels. 
+When moisture is below the threshold (500), it moves the Servo to 45 degrees for 2 seconds. 
+The sensor is connected to A0, and the Servo is connected to pin 9 with an initial position of 90 degrees.
+*/
+
+#include <Servo.h>
+
+const int soilMoistureSensorPin = A0;
+const int servoPin = 9;
+Servo myservo;
+int moistureThreshold = 500; // Adjust this threshold based on your sensor readings
+
+void setup() {
+  pinMode(soilMoistureSensorPin, INPUT);
+  myservo.attach(servoPin);
+  myservo.write(90);  // Move the servo to its initial position (90 degrees)
+  Serial.begin(9600);
+}
+
+void loop() {
+  int moistureLevel = analogRead(soilMoistureSensorPin);
+  if (moistureLevel < moistureThreshold) {
+    Serial.println("Soil moisture is low!");
+    moveServoToPosition(45);
+    delay(2000);  // Adjust the delay time to control how long the servo stays at 45 degrees
+  }
+}
+
+void moveServoToPosition(int angle) {
+  myservo.write(angle);
+}
